dae48a9c73fb1536c15bdb393b66192d83ce3600
Nina Schoppe <nina_schoppe@outlook.de>
2022-01-24 11:25:50 +0100

    add documentation

--- src/main/java/org/wahlzeit/model/Landscape.java
+++ src/main/java/org/wahlzeit/model/Landscape.java
@@ -7,6 +7,7 @@ public class Landscape {
     private final LandscapeType landscapeType;
     private final int id;
 
+    // constructor thet creates a Landscape instance
     public Landscape(LandscapeType landscapeType) {
         if(landscapeType == null) {
             throw new IllegalArgumentException("Landscape type must not be null.");
